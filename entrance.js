6d270c5658c2572aadee51505d78a85969b242be
hsjoihs <hs.ioling.hs@gmail.com>
2019-09-10 21:50:05 +0900

    Implement canceling, though it is unstable

--- entrance.js
+++ entrance.js
@@ -1,29 +1,62 @@
 "use strict";
+let UNLOAD_TRIGGERED_BY_USER = true;
+window.addEventListener('beforeunload', function (e) {
+    if (!UNLOAD_TRIGGERED_BY_USER) {
+        // beforeunload should only capture what has been triggered by the user
+        return;
+    }
+    (async () => {
+        if (typeof RESULT !== "undefined") { // you already have an access token
+            const r = await sendCancel(RESULT.access_token, a => a);
+            if (!r.legal) {
+                alert(`sending cancel somehow resulted in an error: ${r.whyIllegal}`);
+                throw new Error(`sending cancel somehow resulted in an error: ${r.whyIllegal}`);
+            }
+            if (r.cancellable) { // successfully cancelled; you can exit the page
+                return;
+            }
+            else {
+                e.preventDefault();
+                e.returnValue = 'cannot cancel the game, as it is already ready';
+            }
+        }
+    })();
+});
+async function sendCancel(access_token, validateInput) {
+    return await sendSomethingSomewhere('http://localhost:23564/random/cancel', {
+        "access_token": access_token
+    }, validateInput);
+}
 function let_the_game_begin(access_token) {
     alert("Let the game begin");
 }
+let RESULT = undefined;
 function apply_for_random_game() {
     (async () => {
         let res = await sendEntrance(a => a);
+        RESULT = res;
         while (res.state != "let_the_game_begin") {
             await new Promise(resolve => setTimeout(resolve, (2 + Math.random()) * 200 * 0.8093));
             const newRes = await sendPoll(res.access_token, a => a);
             if (newRes.legal) {
                 res = newRes.ret;
+                RESULT = res;
             }
             else {
                 // re-entry
                 res = await sendEntrance(a => a);
+                RESULT = res;
             }
         }
         let_the_game_begin(res.access_token);
     })();
 }
 async function sendPoll(access_token, validateInput) {
-    let url = 'http://localhost:23564/random/poll';
-    const data = {
+    return await sendSomethingSomewhere('http://localhost:23564/random/poll', {
         "access_token": access_token
-    };
+    }, validateInput);
+}
+async function sendSomethingSomewhere(url, data, validateInput) {
     const res = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
@@ -45,25 +78,5 @@ async function sendPoll(access_token, validateInput) {
     return res;
 }
 async function sendEntrance(validateInput) {
-    let url = 'http://localhost:23564/random/entry';
-    const data = {};
-    const res = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function (res) {
-        return res.json();
-    }).then(validateInput)
-        .catch(function (error) {
-        console.error('Error:', error);
-        return;
-    });
-    console.log(res);
-    if (!res) {
-        alert("network error!");
-        throw new Error("network error!");
-    }
-    return res;
+    return await sendSomethingSomewhere('http://localhost:23564/random/entry', {}, validateInput);
 }
--- entrance.ts
+++ entrance.ts
@@ -1,21 +1,68 @@
 import Res_RandomEntry = type__message.Ret_RandomEntry;
 import Res_RandomPoll = type__message.Ret_RandomPoll;
+
+let UNLOAD_TRIGGERED_BY_USER: boolean = true;
+
+type Ret_RandomCancel = {
+    legal: false;
+    whyIllegal: string
+} | {
+    legal: true,
+    cancellable: boolean
+}
+
+window.addEventListener('beforeunload', function (e) {
+    if (!UNLOAD_TRIGGERED_BY_USER) {
+        // beforeunload should only capture what has been triggered by the user
+        return;
+    }
+
+    (async () => {
+        if (typeof RESULT !== "undefined") { // you already have an access token
+            const r = await sendCancel<Ret_RandomCancel>(RESULT.access_token as AccessToken, a => a);
+            if (!r.legal) {
+                alert(`sending cancel somehow resulted in an error: ${r.whyIllegal}`);
+                throw new Error(`sending cancel somehow resulted in an error: ${r.whyIllegal}`);
+            }
+
+            if (r.cancellable) { // successfully cancelled; you can exit the page
+                return;
+            } else {
+                e.preventDefault();
+                e.returnValue = 'cannot cancel the game, as it is already ready';
+            }
+        }
+    })();
+});
+
+async function sendCancel<U>(access_token: AccessToken, validateInput: (response: any) => U): Promise<U> {
+    return await sendSomethingSomewhere('http://localhost:23564/random/cancel', {
+        "access_token": access_token
+    }, validateInput);
+}
+
+
 type AccessToken = string & { __AccessTokenBrand: never };
 function let_the_game_begin(access_token: AccessToken) {
     alert("Let the game begin");
 }
 
+let RESULT: Res_RandomEntry | undefined = undefined;
+
 function apply_for_random_game() {
     (async () => {
         let res: Res_RandomEntry = await sendEntrance<Res_RandomEntry>(a => a);
+        RESULT = res;
         while (res.state != "let_the_game_begin") {
             await new Promise(resolve => setTimeout(resolve, (2 + Math.random()) * 200 * 0.8093));
             const newRes: Res_RandomPoll = await sendPoll<Res_RandomPoll>(res.access_token as AccessToken, a => a);
             if (newRes.legal) {
                 res = newRes.ret;
+                RESULT = res;
             } else {
                 // re-entry
                 res = await sendEntrance<Res_RandomEntry>(a => a);
+                RESULT = res;
             }
         }
         let_the_game_begin(res.access_token as AccessToken);
@@ -23,11 +70,12 @@ function apply_for_random_game() {
 }
 
 async function sendPoll<U>(access_token: AccessToken, validateInput: (response: any) => U): Promise<U> {
-    let url = 'http://localhost:23564/random/poll';
-    const data = {
+    return await sendSomethingSomewhere('http://localhost:23564/random/poll', {
         "access_token": access_token
-    };
+    }, validateInput);
+}
 
+async function sendSomethingSomewhere<T, U>(url: string, data: T, validateInput: (response: any) => U): Promise<U> {
     const res: void | U = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(data), // data can be `string` or {object}!
@@ -51,30 +99,6 @@ async function sendPoll<U>(access_token: AccessToken, validateInput: (response: 
     return res;
 }
 
-
 async function sendEntrance<U>(validateInput: (response: any) => U): Promise<U> {
-    let url = 'http://localhost:23564/random/entry';
-    const data = {};
-
-    const res: void | U = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function (res) {
-        return res.json();
-    }).then(validateInput)
-        .catch(function (error) {
-            console.error('Error:', error);
-            return;
-        });
-
-    console.log(res);
-
-    if (!res) {
-        alert("network error!");
-        throw new Error("network error!");
-    }
-    return res;
+    return await sendSomethingSomewhere('http://localhost:23564/random/entry', {}, validateInput);
 }
