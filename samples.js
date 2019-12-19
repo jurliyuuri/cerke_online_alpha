633e1599c265db85a6982868f2e9e5f649ff542d
hsjoihs <hs.ioling.hs@gmail.com>
2019-12-19 19:45:49 +0900

    Initial position

--- samples.js
+++ samples.js
@@ -15,20 +15,20 @@ var eightNeighborhood = calculate_movable.eightNeighborhood;
 var isTamHue = calculate_movable.isTamHue;
 var canGetOccupiedBy = calculate_movable.canGetOccupiedBy;
 const sampleBoard = [
-    [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward },
-        null, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, null, { color: Color.Kok1, prof: Profession.Gua2, side: Side.Upward }, null, null],
-    [null, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, null, null, { color: Color.Kok1, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kua2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, null],
-    [{ color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, "Tam2", "Tam2", null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Uai1, side: Side.Downward }, null, null],
-    [null, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, "Tam2", null, null, null, null, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward }, null],
-    [null, null, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Downward }, null, null],
-    [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward },
-        { color: Color.Kok1, prof: Profession.Gua2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null, null, { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Upward }, null, null],
-    [null, "Tam2", null, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, null],
-    [null, null, { color: Color.Kok1, prof: Profession.Io, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Upward }, null, null, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, null],
-    [null, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Maun1, side: Side.Upward }, null, null, null, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, "Tam2"],
+    [{ color: Color.Huok2, prof: Profession.Kua2, side: Side.Downward },
+        { color: Color.Huok2, prof: Profession.Maun1, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Io, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Uai1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kaun1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Maun1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kua2, side: Side.Downward }],
+    [{ color: Color.Kok1, prof: Profession.Tuk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Gua2, side: Side.Downward }, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Downward }, null, { color: Color.Huok2, prof: Profession.Gua2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Tuk2, side: Side.Downward }],
+    [{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Nuak1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, "Tam2", null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }],
+    [{ color: Color.Huok2, prof: Profession.Tuk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Gua2, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Gua2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }],
+    [{ color: Color.Kok1, prof: Profession.Kua2, side: Side.Upward },
+        { color: Color.Kok1, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kaun1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Uai1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Io, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kua2, side: Side.Upward }],
 ];
 const sampleField = {
     currentBoard: sampleBoard,
-    hop1zuo1OfDownward: [{ color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward }],
-    hop1zuo1OfUpward: [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }],
+    hop1zuo1OfDownward: [],
+    hop1zuo1OfUpward: [],
 };
--- samples.ts
+++ samples.ts
@@ -31,21 +31,21 @@ import isTamHue = calculate_movable.isTamHue;
 import canGetOccupiedBy = calculate_movable.canGetOccupiedBy;
 
 const sampleBoard: Board = [
-    [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward },
-        null, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, null, { color: Color.Kok1, prof: Profession.Gua2, side: Side.Upward }, null, null],
-    [null, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, null, null, { color: Color.Kok1, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kua2, side: Side.Upward }, {color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward}, null],
-    [{ color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, "Tam2", "Tam2", null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, {color: Color.Kok1, prof: Profession.Uai1, side: Side.Downward}, null, null],
-    [null, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, "Tam2", null, null, null, null, {color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward}, null],
-    [null, null, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null,  { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Downward }, null, null],
-    [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward },
-        {color: Color.Kok1, prof: Profession.Gua2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null, null, { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Upward }, null, null],
-    [null, "Tam2", null, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }, null],
-    [null, null, { color: Color.Kok1, prof: Profession.Io, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Upward }, null, null, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, null],
-    [null, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Maun1, side: Side.Upward }, null, null, null, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, "Tam2"],
+    [{ color: Color.Huok2, prof: Profession.Kua2, side: Side.Downward },
+        { color: Color.Huok2, prof: Profession.Maun1, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Io, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Uai1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kaun1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Maun1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kua2, side: Side.Downward }],
+    [{ color: Color.Kok1, prof: Profession.Tuk2, side: Side.Downward }, {color: Color.Kok1, prof: Profession.Gua2, side: Side.Downward }, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Downward }, null, {color: Color.Huok2, prof: Profession.Gua2, side: Side.Downward },{ color: Color.Huok2, prof: Profession.Tuk2, side: Side.Downward }],
+    [{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward },{ color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Nuak1, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Downward },{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Downward }],
+    [null,null, null, null, null, null, null, null, null],
+    [null, null, null, null, "Tam2", null, null, null, null],
+    [null,null, null, null, null, null, null, null, null],
+    [{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward },{ color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Nuak1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kauk2, side: Side.Upward },{ color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }],
+    [{ color: Color.Huok2, prof: Profession.Tuk2, side: Side.Upward }, {color: Color.Huok2, prof: Profession.Gua2, side: Side.Upward }, null, { color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, null, { color: Color.Kok1, prof: Profession.Dau2, side: Side.Upward }, null, {color: Color.Kok1, prof: Profession.Gua2, side: Side.Upward },{ color: Color.Kok1, prof: Profession.Tuk2, side: Side.Upward }],
+    [{ color: Color.Kok1, prof: Profession.Kua2, side: Side.Upward },
+        { color: Color.Kok1, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Kaun1, side: Side.Upward }, { color: Color.Kok1, prof: Profession.Uai1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Io, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kaun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Maun1, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kua2, side: Side.Upward }],
 ];
 
 const sampleField: Field = {
     currentBoard: sampleBoard,
-    hop1zuo1OfDownward: [{ color: Color.Kok1, prof: Profession.Dau2, side: Side.Downward }, { color: Color.Huok2, prof: Profession.Uai1, side: Side.Downward }],
-    hop1zuo1OfUpward: [{ color: Color.Huok2, prof: Profession.Dau2, side: Side.Upward }, { color: Color.Huok2, prof: Profession.Kauk2, side: Side.Upward }],
+    hop1zuo1OfDownward: [],
+    hop1zuo1OfUpward: [],
 };
