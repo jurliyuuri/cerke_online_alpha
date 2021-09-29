f36125325c69756d4250246743dcbf35cad186df
hsjoihs <hs.ioling.hs@gmail.com>
2021-09-29 22:43:54 +0900

    Hmmm

--- src/kiar_ark.ts
+++ src/kiar_ark.ts
@@ -6,6 +6,7 @@ const _kiar_ark: {
 export const KIAR_ARK = new Proxy(_kiar_ark, {
     set: function (target, p: "header" | "body", value: string[]) {
         target[p] = value;
+        console.log("_kiar_ark:", _kiar_ark);
         document.getElementById("kiar_ark")!.innerHTML = _kiar_ark.header.join("\n") + "\n" + _kiar_ark.body.join("\n")
         return true;
     }
