919cd6e79b719ada884dfd592aacfba748df4c36
hsjoihs <hs.ioling.hs@gmail.com>
2021-09-30 00:06:51 +0900

    Render by grouping two moves in a single line

--- src/kiar_ark.ts
+++ src/kiar_ark.ts
@@ -3,11 +3,19 @@ const _kiar_ark: {
     body: string[]
 } = { header: [], body: [] };
 
+function groupTwoAndRender(input: string[]) {
+    let ans: string = "";
+    for (let i = 0; i < input.length; i += 2) {
+        ans += (input[i] ?? "") + "\t" + (input[i+1] ?? "") + "\n";
+    }
+    return ans;
+}
+
 export const KIAR_ARK = new Proxy(_kiar_ark, {
     set: function (target, p: "header" | "body", value: string[]) {
         target[p] = value;
         console.log("_kiar_ark:", _kiar_ark);
-        document.getElementById("kiar_ark")!.innerHTML = _kiar_ark.header.join("\n") + "\n" + _kiar_ark.body.join("\n")
+        document.getElementById("kiar_ark")!.innerHTML = _kiar_ark.header.join("\n") + "\n" + groupTwoAndRender(_kiar_ark.body)
         return true;
     }
 })
