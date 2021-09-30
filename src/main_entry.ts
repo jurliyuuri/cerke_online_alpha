9a39e1948a48cc3107b88991e05c9c39a3fb451c
hsjoihs <hs.ioling.hs@gmail.com>
2021-10-01 08:22:18 +0900

    Fix

--- src/main_entry.ts
+++ src/main_entry.ts
@@ -47,7 +47,9 @@ document.getElementById("kait_kaik_button")!.addEventListener("click", () => {
   document.getElementById("kait_kaik")!.classList.add("nocover");
   GAME_STATE.is_my_turn = JSON.parse(sessionStorage.is_first_move_my_move);
   KIAR_ARK.header = [...KIAR_ARK.header, { type: "header", dat: `{一位色:${GAME_STATE.is_my_turn === GAME_STATE.IA_is_down ? "黒" : "赤"}}` }];
-  toggleBackgroundMusic();
+  if (!LORK_LIAR_ENABLED) {
+    toggleBackgroundMusic();
+  }
 });
 
 if (sessionStorage.vs === "cpu") {
