a3d617ed86c5727c51d020e39a67ced130fee99c
hsjoihs <hs.ioling.hs@gmail.com>
2021-10-16 06:37:47 +0900

    Add translations to the tactics

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -3,7 +3,28 @@ export interface DictElem {
   failedWaterEntry: string;
   gameEnd: string;
   newSeason: string[];
+  tactics: Tactics
 }
+
+export type Tactics = {
+  victory_almost_certain: string,
+  strengthened_shaman: string,
+  free_lunch: string,
+  avoid_defeat: string,
+  loss_almost_certain: string,
+  neutral: string,
+}
+
+export const TACTICS_LINZKLAR: Tactics = {
+  victory_almost_certain: "我須上行。",
+  strengthened_shaman: "為激巫。",
+  free_lunch: "我為無与之手。",
+  avoid_defeat: "心為此而無行下行之道。",
+  loss_almost_certain: "為何即下行。行我心之道。",
+  neutral: "無心来為何善。行周時無下行之道。"
+}
+
+export type TacticsKey = keyof Tactics;
 
 export interface Dictionary {
   ja: DictElem;
@@ -20,5 +41,13 @@ export const DICTIONARY: Dictionary = {
       "季節が改まり冬になりました。",
     ],
     gameEnd: "試合終了です。",
+    tactics: {
+      victory_almost_certain: "勝ち確",
+      strengthened_shaman: "激巫作成",
+      free_lunch: "ただ取り",
+      avoid_defeat: "負けを避けるためにこう指してみるか",
+      loss_almost_certain: "なにやっても負けそうなので好き勝手に指す",
+      neutral: "いい手が思いつかなかったので、即負けしない範囲で好き勝手に指す"
+    }
   },
 };
