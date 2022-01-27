38c8e09a30d93d831e82f7c40c913c39ed42e70e
hsjoihs <hs.ioling.hs@gmail.com>
2022-01-28 01:37:00 +0900

    Fix; MEMBRANE_STATE cannot be set before being set

--- src/protective_cover.ts
+++ src/protective_cover.ts
@@ -17,10 +17,22 @@ const MEMBRANE_STATE: MembraneState
 	;
 
 function apply_to_dom_and_return(m: MembraneState) {
-	if (m["protective_cover_over_field"]) { add_cover("protective_cover_over_field") } else { remove_cover("protective_cover_over_field") }
-	if (m["protective_tam_cover_over_field"]) { add_cover("protective_tam_cover_over_field") } else { remove_cover("protective_tam_cover_over_field") }
-	if (m["protective_cover_over_field_while_asyncawait"]) { add_cover("protective_cover_over_field_while_asyncawait") } else { remove_cover("protective_cover_over_field_while_asyncawait") }
-	if (m["protective_cover_over_field_while_waiting_for_opponent"]) { add_cover("protective_cover_over_field_while_waiting_for_opponent") } else { remove_cover("protective_cover_over_field_while_waiting_for_opponent") }
+	function apply(id:
+		| "protective_cover_over_field"
+		| "protective_tam_cover_over_field"
+		| "protective_cover_over_field_while_asyncawait"
+		| "protective_cover_over_field_while_waiting_for_opponent") {
+		if (m[id]) {
+			document.getElementById(id)?.classList.remove("nocover");
+		} else {
+			document.getElementById(id)?.classList.add("nocover");
+		}
+	}
+
+	apply("protective_cover_over_field")
+	apply("protective_tam_cover_over_field")
+	apply("protective_cover_over_field_while_asyncawait")
+	apply("protective_cover_over_field_while_waiting_for_opponent")
 	return m;
 }
 
