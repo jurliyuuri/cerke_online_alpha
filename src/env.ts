1d87c3c18782ba3a043f05a3e624f1ebaee101ca
hsjoihs <hs.ioling.hs@gmail.com>
2022-08-31 21:15:51 +0900

    How about HTTPS?

--- src/env.ts
+++ src/env.ts
@@ -7,4 +7,4 @@ declare const process: {
 export const API_ORIGIN =
   process.env.API_ORIGIN === "LOCAL"
     ? "http://localhost:23564"
-    : "http://little-water-8645.fly.dev";
+    : "https://little-water-8645.fly.dev";
